9f0061ca6ff2b324898cf759ae4e98cfe867bb8d
gezonthenet <gerard21@gmail.com>
2023-01-30 12:28:03 +1100

    Remove angular2csv from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Angular2CsvModule } from 'angular2-csv';
 import { CompressionService } from './compression.service';
 import { ChartsModule  } from 'ng2-charts';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -18,7 +17,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     ChartsModule,
     HttpClientModule,
-    Angular2CsvModule,
     FormsModule,
     ReactiveFormsModule
   ],

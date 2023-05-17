8e394a8df5ef35b7fcb5e622fc6985eedea2c46b
gezonthenet <gerard21@gmail.com>
2023-05-17 14:10:03 +1000

    Updated label pipe logic.

--- src/app/label.pipe.ts
+++ src/app/label.pipe.ts
@@ -6,7 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LabelPipe implements PipeTransform {
 
   transform(value: string): string {
-    if (value === 'temperature') return 'temp';
+    if (value.match(/temperature/)) return value.replace(/temperature/g, String.fromCharCode(176) + 'C');
+    if (value.match(/humidity/)) return value.replace(/humidity/g, '%');
     return value;
   }
 

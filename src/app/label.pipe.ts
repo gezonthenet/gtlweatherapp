54f8c34f52493da2bd7a71aded71deed4ac74c76
gezonthenet <gerard21@gmail.com>
2023-05-17 15:11:45 +1000

    Another update to label logic.

--- src/app/label.pipe.ts
+++ src/app/label.pipe.ts
@@ -5,10 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class LabelPipe implements PipeTransform {
 
-  transform(value: string): string {
-    if (value.match(/temperature/)) return value.replace(/temperature/g, String.fromCharCode(176) + 'C');
-    if (value.match(/humidity/)) return value.replace(/humidity/g, '%');
-    return value;
+  transform(value: string, label?:string): string {
+    if (label) {
+      if (label.match(/temperature/)) return value + String.fromCharCode(176) + 'C';
+      if (label.match(/humidity/)) return value+'%';
+      if (label.match(/daily/)) return value+' mm';
+      if (label.match(/rate/)) return value+' mm/h';
+      if (label.match(/moisture/)) return value+'%';
+      return value;
+    } else {
+      if (value.match(/temperature/)) return value.replace(/temperature/g, '');
+      if (value.match(/humidity/)) return value.replace(/humidity/g, '');
+      //if (value.match(/temperature/)) return value.replace(/temperature/g, String.fromCharCode(176) + 'C');
+      //if (value.match(/humidity/)) return value.replace(/humidity/g, '%');
+      return value;
+    }
   }
 
 }
